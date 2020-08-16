5d085bd51eb0086a9d910ea13031c6e5c40bf5ff
Kyle Hicks <kylekhicks@gmail.com>
2020-08-16 17:25:27 -0500

    still trying

--- hello/models.py
+++ hello/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Subscribe(models.Model):
     email = models.EmailField()
-    date_added = models.DateTimeField("date added", auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.email
     
